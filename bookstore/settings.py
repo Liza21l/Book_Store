715d74769348a24033ac7a6986dabbbf0245610f
Liza21l <Liza2009koshenya@gmail.com>
2026-02-05 19:38:08 +0200

    add secret_key to gitignore

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9c%596k4#!t@@v3(3gr4k+wy3aen_+gdjng@ww0irxa#+*jiep'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
