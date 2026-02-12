0a176b943ee26c8cc740ce2b11894360cc410450
Liza21l <Liza2009koshenya@gmail.com>
2026-02-12 18:10:08 +0200

    Add design

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order, OrderItem
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -7,3 +7,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['genre']
     search_fields = ("title", "author")
 
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "created_at", "total_price", "status")
+    list_filter = ("status", "created_at")
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
